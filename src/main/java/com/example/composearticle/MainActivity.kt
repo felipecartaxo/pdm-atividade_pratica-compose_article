2fffc95f1b491eb09f297ccf36360f2467e9657e
Neraky <felipecartaxo1@gmail.com>
2025-01-26 23:00:50 -0300

    criando repositorio

---
+++ src/main/java/com/example/composearticle/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composearticle.ui.theme.ComposeArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeArticleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticle()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticle() {
+    val titulo = stringResource(R.string.text_tittle)
+    val descricao = stringResource(R.string.text_shortDescription)
+    val descricaoCompleta = stringResource(R.string.text_longDescription)
+    val imagem = painterResource(R.drawable.bg_compose_background)
+
+    Column {
+        Image(painter = imagem, contentDescription = null)
+        Text(
+            text = titulo,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = descricao,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = descricaoCompleta,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeArticlePreview() {
+    ComposeArticleTheme {
+        ComposeArticle()
+    }
+}
